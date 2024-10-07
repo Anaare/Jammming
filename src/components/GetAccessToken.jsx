37f74c9f649215d7cef2b02f4bd94ac809e24352
Anaare <arevadzeana10@gmail.com>
2024-10-07 17:51:43 +0400

    add access token generate function

---
+++ src/components/GetAccessToken.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export default function GetAccessToken() {
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    const getSpotifyToken = async () => {
+      const clientId = "4150329a47d1455e95cdff00a1991331";
+      const clientSecret = "502f6e628ecf4de58f12c0f9d4b70c54";
+
+      const params = new URLSearchParams();
+      params.append("grant_type", "client_credentials");
+      params.append("client_id", clientId);
+      params.append("client_secret", clientSecret);
+
+      try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: params,
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to get access token");
+        }
+
+        const data = await response.json();
+        setAccessToken(data.access_token);
+        console.log("Access Token:", data.access_token);
+      } catch (error) {
+        console.error("Error getting Spotify access token:", error);
+      }
+    };
+
+    getSpotifyToken();
+  }, []);
+
+  return (
+    <>
+      <h1>{accessToken}</h1>
+    </>
+  );
+}
